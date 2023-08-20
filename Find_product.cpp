0516d2c8fdeadbde90d2be87e301fde2e00f1015
Vaishali <vaishalisaini0203@gmail.com>
2023-08-20 22:04:33 +0530

    Create Find_product.cpp

---
+++ Find_product.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long int ans;
+    int n;
+    cin >> n;
+    int a[n], i;
+    for(i=0; i<n; i++)
+    {
+    	cin >> a[i];
+    }
+    ans=1;
+    for(i=0; i<n; i++)
+    {
+    	ans = (ans*a[i]) % (1000000007);
+    }
+    cout << ans;
+    return 0;
+}
